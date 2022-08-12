0494557fa0d3d5e086a0ec93969d950f75cf846c
ale183 <ale183.dev@gmail.com>
2022-08-12 19:31:20 +0200

    Updated to bot API 6.2

--- lib/src/entities/telegram/sticker_set.dart
+++ lib/src/entities/telegram/sticker_set.dart
@@ -6,18 +6,18 @@ import '../internal/helpers/util.dart';
 class StickerSet {
   String name;
   String title;
+  String stickerType;
   bool isAnimated;
   bool isVideo;
-  bool containsMasks;
   List<Sticker> stickers;
   PhotoSize? thumb;
 
   StickerSet({
     required this.name,
     required this.title,
+    required this.stickerType,
     required this.isAnimated,
     required this.isVideo,
-    required this.containsMasks,
     required this.stickers,
     this.thumb,
   });
@@ -26,9 +26,9 @@ class StickerSet {
     return StickerSet(
       name: json['name']!,
       title: json['title']!,
+      stickerType: json['sticker_type'],
       isAnimated: json['is_animated']!,
       isVideo: json['is_video']!,
-      containsMasks: json['contains_masks']!,
       stickers: Sticker.listFromJsonArray(json['stickers']!),
       thumb: callIfNotNull(PhotoSize.fromJson, json['thumb']),
     );
@@ -38,9 +38,9 @@ class StickerSet {
     return {
       'name': name,
       'title': title,
+      'sticker_type': stickerType,
       'is_animated': isAnimated,
       'is_video': isVideo,
-      'contains_masks': containsMasks,
       'stickers': stickers,
       'photo_size': thumb,
     }..removeWhere((_, v) => v == null);
