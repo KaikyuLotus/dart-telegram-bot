6ae3dc9911355635d931780b645218f0863bd893
ale183 <ale183.dev@gmail.com>
2023-02-05 13:10:02 +0100

    ChatPermissions update

--- lib/src/entities/telegram/chat_permissions.dart
+++ lib/src/entities/telegram/chat_permissions.dart
@@ -8,10 +8,23 @@ class ChatPermissions {
   /// and venues
   bool? canSendMessages;
 
-  /// Optional.
-  /// True, if the user is allowed to send audios, documents, photos, videos,
-  /// video notes and voice notes, implies can_send_messages
-  bool? canSendMediaMessages;
+  /// True, if the user is allowed to send audios
+  bool? canSendAudios;
+
+  /// True, if the user is allowed to send documents
+  bool? canSendDocuments;
+
+  /// True, if the user is allowed to send photos
+  bool? canSendPhotos;
+
+  /// True, if the user is allowed to send videos
+  bool? canSendVideos;
+
+  /// True, if the user is allowed to send video notes
+  bool? canSendVideoNotes;
+
+  /// True, if the user is allowed to send voice notes
+  bool? canSendVoiceNotes;
 
   /// Optional.
   /// True, if the user is allowed to send polls, implies can_send_messages
@@ -48,7 +61,12 @@ class ChatPermissions {
   /// Basic constructor
   ChatPermissions({
     this.canSendMessages,
-    this.canSendMediaMessages,
+    this.canSendAudios,
+    this.canSendDocuments,
+    this.canSendPhotos,
+    this.canSendVideos,
+    this.canSendVideoNotes,
+    this.canSendVoiceNotes,
     this.canSendPolls,
     this.canSendOtherMessages,
     this.canAddWebPagePreviews,
@@ -62,7 +80,12 @@ class ChatPermissions {
   static ChatPermissions fromJson(Map<String, dynamic> json) {
     return ChatPermissions(
       canSendMessages: json['can_send_messages'],
-      canSendMediaMessages: json['can_send_media_messages'],
+      canSendAudios: json['can_send_audios'],
+      canSendDocuments: json['can_send_documents'],
+      canSendPhotos: json['can_send_photos'],
+      canSendVideos: json['can_send_videos'],
+      canSendVideoNotes: json['can_send_video_notes'],
+      canSendVoiceNotes: json['can_send_voice_notes'],
       canSendPolls: json['can_send_polls'],
       canSendOtherMessages: json['can_send_other_messages'],
       canAddWebPagePreviews: json['can_add_web_page_previews'],
@@ -77,7 +100,12 @@ class ChatPermissions {
   Map toJson() {
     return {
       'can_send_messages': canSendMessages,
-      'can_send_media_messages': canSendMediaMessages,
+      'can_send_audios': canSendAudios,
+      'can_send_documents': canSendDocuments,
+      'can_send_photos': canSendPhotos,
+      'can_send_videos': canSendVideos,
+      'can_send_video_notes': canSendVideoNotes,
+      'can_send_voice_notes': canSendVoiceNotes,
       'can_send_polls': canSendPolls,
       'can_send_other_messages': canSendOtherMessages,
       'can_add_web_page_previews': canAddWebPagePreviews,
